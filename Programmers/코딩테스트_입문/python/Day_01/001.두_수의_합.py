55e1f1ef457038b40b688a2c292c587f3d8d8e97
SoularOfDawn <tntkala001@gmail.com>
2022-10-26 23:48:48 +0900

    pro_basic_py 01-001

---
+++ Programmers/코딩테스트_입문/python/Day_01/001.두_수의_합.py
@@ -0,0 +1,10 @@
+# url : https://school.programmers.co.kr/learn/courses/30/lessons/120802
+
+
+'''
+문제 : 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 solution 함수를 완성해주세요.
+
+'''
+
+def solution(num1, num2):
+  return num1 + num2
