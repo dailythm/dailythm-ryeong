287cbd03bcc21dfd63e21ffc9fa919121775a980
SoularOfDawn <tntkala001@gmail.com>
2022-10-26 23:53:13 +0900

    pro_basic_py 01-002

---
+++ Programmers/CodingTest-basic/python/Day-01/002.두_수의_차.py
@@ -0,0 +1,10 @@
+# url : https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+
+'''
+문제 : 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 solution 함수를 완성해주세요.
+
+'''
+
+def solution(num1, num2):
+  return num1 - num2
