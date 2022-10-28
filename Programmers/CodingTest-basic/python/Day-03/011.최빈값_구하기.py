4c239b6ab22e4361a9ed73dae8c77315574ae28c
SoularOfDawn <tntkala001@gmail.com>
2022-10-28 20:53:28 +0900

    pro_basic_py 03-011

---
+++ Programmers/CodingTest-basic/python/Day-03/011.최빈값_구하기.py
@@ -0,0 +1,20 @@
+# url : https://school.programmers.co.kr/learn/courses/30/lessons/120812
+
+
+'''
+문제 : 최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다. 
+      정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요. 
+      최빈값이 여러 개면 -1을 return 합니다.
+
+'''
+
+def solution(array):
+    max = [0,0]
+    for i in set(array):
+        count = array.count(i)
+        if(count > max[0]):
+            max[0] = count
+            max[1] = i
+        elif(count == max[0]):
+            max[1] = -1
+    return max[1]
