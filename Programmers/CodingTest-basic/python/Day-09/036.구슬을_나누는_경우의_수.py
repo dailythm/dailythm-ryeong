cdfa0771e576134879f6a567941e37ed9d33cd74
SoularOfDawn <tntkala001@gmail.com>
2022-11-04 09:36:53 +0900

    pro_basic_py 09-033~036 + done

---
+++ Programmers/CodingTest-basic/python/Day-09/036.구슬을_나누는_경우의_수.py
@@ -0,0 +1,21 @@
+# url : https://school.programmers.co.kr/learn/courses/30/lessons/120840
+
+
+'''
+문제 : 머쓱이는 구슬을 친구들에게 나누어주려고 합니다. 구슬은 모두 다르게 생겼습니다. 
+      머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때, 
+      balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+
+'''
+
+def solution(balls, share):
+    answer, big, small = 1, 0, 0 
+    
+    if share > balls - share: 
+        big, small = share, balls - share
+    else: big, small = balls - share, share
+    
+    for i in range(big +1, balls +1): answer *= i
+    for i in range(1, small +1): answer /= i
+    
+    return answer
